3763a3b13d6fea257c28c937ce5bc82ec6eb762a
Ab Sabury <sabury2020@gmail.com>
2022-10-15 10:22:45 -0700

    project completed

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+let btnShowModal = document.querySelectorAll('.show-modal');
+let btnCloseModal = document.querySelector('.close-modal');
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+
+const showModal = function (){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const hiddeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let index = 0; index < btnShowModal.length; index++)
+    btnShowModal[index].addEventListener('click',showModal);
+
+
+    overlay.addEventListener('click',hiddeModal);
+    btnCloseModal.addEventListener('click',hiddeModal);
